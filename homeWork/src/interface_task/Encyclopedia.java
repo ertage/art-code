943c91a0ed3c2ed9976c50ea81982082e9291dc7
Nastia <Nastia@AnastasiiaShvetsova.local>
2017-01-24 22:55:44 +0200

    task Interface

---
+++ homeWork/src/interface_task/Encyclopedia.java
@@ -0,0 +1,8 @@
+package interface_task;
+
+/**
+ * Created by Nastia on 24.01.17.
+ */
+public class Encyclopedia extends Book{
+
+}
---
+++ homeWork/src/interface_task/Printable.java
@@ -0,0 +1,9 @@
+package interface_task;
+
+/**
+ * Created by Nastia on 24.01.17.
+ */
+public interface Printable {
+
+    void printBook();
+}
